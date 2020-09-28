894419686af5331c113445076d7de27c0f33421e
wltnet <wltnet@hotmail.com>
2020-09-28 19:59:26 +0100

    update News unit test

--- src/components/News/News.test.tsx
+++ src/components/News/News.test.tsx
@@ -3,6 +3,7 @@ import { shallow } from "enzyme";
 import News from "./News";
 import Button from "../Button";
 import NewsCard from "../NewsCard";
+import Dropdown from "../Dropdown";
 import mockData from "../../util/mockData";
 
 jest.mock("../../util/useFetch", () => jest.fn(() => mockData));
@@ -12,6 +13,29 @@ describe("<News />", () => {
     const wrapper = shallow(<News />);
 
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("should not have duplicate source in dropdown", () => {
+    const source = [
+      "New York Times",
+      "The Wall Street Journal",
+      "Teslarati",
+      "CNET",
+      "Yahoo Entertainment",
+      "CarScoops",
+      "Bloomberg",
+      "CNBC",
+      "CNN",
+      "Associated Press",
+      "WRAL.com",
+      "InsideEVs ",
+      "Hawaii News Now",
+      "USA Today",
+      "WJHL-TV News Channel 11",
+    ];
+
+    const wrapper = shallow(<News />);
+    expect(wrapper.find(Dropdown).prop("list")).toEqual(source);
   });
 
   it("should show 5 more news after click show more button", () => {
